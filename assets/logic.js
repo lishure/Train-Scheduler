07b61e5bb3854a6effd11cfd0189317a8c467fbc
Lishure <HUEY_LISA@YAHOO.COM>
2019-04-06 00:19:36 -0700

    added html layout and psuedocode

---
+++ assets/logic.js
@@ -0,0 +1,9 @@
+
+//Need to have form and submit button
+//Form name should include: Train Name, Destination, 
+//First Train Time -- in military time, Frequency -- in minutes
+//Need to be able to store and push text into html display for relevant fields
+//Need to be able to sync up train time with realtime
+//current wait time should reflect with train interval and real time
+//Need to have click function to start timer when button clicked for train time
+//Click function should also store and push text into html
