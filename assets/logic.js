192163b54d804a7c13f41f11aa424c69c9ca3df5
Lishure <HUEY_LISA@YAHOO.COM>
2019-04-07 12:53:49 -0700

    added moment js

--- assets/logic.js
+++ assets/logic.js
@@ -1,27 +1,25 @@
 //Initialize firebase
-  var config = {
-    apiKey: "AIzaSyAmCRAqaUL7QpIkPPSn2sqAj9hjCRse8ws",
-    authDomain: "employeeproject-21f77.firebaseapp.com",
-    databaseURL: "https://employeeproject-21f77.firebaseio.com",
-    projectId: "employeeproject-21f77",
-    storageBucket: "employeeproject-21f77.appspot.com",
-    messagingSenderId: "842985455265"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyAmCRAqaUL7QpIkPPSn2sqAj9hjCRse8ws",
+  authDomain: "employeeproject-21f77.firebaseapp.com",
+  databaseURL: "https://employeeproject-21f77.firebaseio.com",
+  projectId: "employeeproject-21f77",
+  storageBucket: "employeeproject-21f77.appspot.com",
+  messagingSenderId: "842985455265"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  //Onclick function to add train when button is clicked
-  document.querySelector("#add-train").addEventListener("click", function(event) {
-    event.preventDefault();
+//Onclick function to add train when button is clicked
+document.querySelector("#add-train").addEventListener("click", function (event) {
+  event.preventDefault();
 
- // Grabs user input
- var trainInput = document.querySelector("#name-input").value.trim();
- var destinationInput = document.querySelector("#destination-input").value.trim();
- //******/Need to Add moment and time structure********
- //********************************************************** */
- var timeInput = document.querySelector("#time-input").value.trim();
- var frequencyInput = document.querySelector("#frequency-input").value.trim();
+  // Grabs user input
+  var trainInput = document.querySelector("#name-input").value.trim();
+  var destinationInput = document.querySelector("#destination-input").value.trim();
+  var timeInput = document.querySelector("#time-input").value.trim();
+  var frequencyInput = document.querySelector("#frequency-input").value.trim();
 
   // Holds train data
   var newTrain = {
@@ -39,40 +37,62 @@
   console.log(newTrain.destination);
   console.log(newTrain.time);
   console.log(newTrain.frequency);
-//alert
+  //alert
   alert("Train successfully added");
 
-    // Need to clear text box when train is added
-    document.querySelector("#name-input").value = "";
-    document.querySelector("#destination-input").value = "";
-    document.querySelector("#time-input").value = "";
-    document.querySelector("#frequency-input").value = "";
-  });
+  // Need to clear text box when train is added
+  document.querySelector("#name-input").value = "";
+  document.querySelector("#destination-input").value = "";
+  document.querySelector("#time-input").value = "";
+  document.querySelector("#frequency-input").value = "";
+});
 
-  // Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-  
-    // Store everything into a variable.
-    var trainInput = childSnapshot.val().train;
-    var destinationInput = childSnapshot.val().destination;
-    var timeInput = childSnapshot.val().time;
-    var frequencyInput = childSnapshot.val().frequency;
-  
-    // Employee Info
-    console.log(trainInput);
-    console.log(destinationInput);
-    console.log(timeInput);
-    console.log(frequencyInput);
+// Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
+  console.log(childSnapshot.val());
 
-     // create a temp object of our values
+  // Store everything into a variable.
+  var trainInput = childSnapshot.val().train;
+  var destinationInput = childSnapshot.val().destination;
+  var timeInput = childSnapshot.val().time;
+  var frequencyInput = childSnapshot.val().frequency;
+
+  // Employee Info
+  console.log(trainInput);
+  console.log(destinationInput);
+  console.log(timeInput);
+  console.log(frequencyInput);
+
+    //******/Need to Add moment and time structure********
+
+  //empty strings for minutesaway until next train
+  var minsAway;
+
+  var firstTrain = moment(timeInput, "hh:mm");
+  // Difference in time between the current and firstTrain
+  var diffTime = moment().diff(moment(firstTrain), "minutes");
+  var remainder = diffTime % frequencyInput;
+  // Minutes until next train
+  var minsAway = frequencyInput - remainder;
+  // Next train time
+  var nextTrain = moment().add(minsAway, "minutes");
+  nextTrain = moment(nextTrain).format("hh:mm");
+
+
+  // create a temp object of our values
   let tempTrainData = {
     train: trainInput,
     destination: destinationInput,
-    frequency: frequencyInput
+    frequency: frequencyInput,
+    nextTrain: nextTrain,
+    minsAway: minsAway
   };
+  console.log(tempTrainData);
 
-var newRow = document.createElement("tr");
+
+  var newRow = document.createElement("tr");
+
+
 
   // Loop through the childSnapshot object
 
